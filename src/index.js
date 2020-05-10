e64d657bfa7ea365a64aa6ccae5dc91e97862673
Manish <manish.m@cogknit.com>
2020-05-10 16:07:03 +0530

    no chnage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
 
-ReactDOM.render(
-  <React.StrictMode>
+import App from "./Components/App";
+import "./index/index.html"
+// Todo
+// import "./assets/bootstrap/customVariables.scss";
+// import "./assets/css/styles.scss";
+
+import reducers from "./store/reducers/index";
+
+//for redux devtools
+const composeEnhancers =
+  typeof window === "object" && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+    : compose;
+
+const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
+
+
+
+ReactDom.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Provider>,
+  document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
